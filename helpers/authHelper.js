9386f4b47862790e5949f83e7eda35b22eae1e19
apekshyaghimire <ghimireapekshya368@gmail.com>
2025-11-05 19:20:56 +0545

    upto login register

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -9,7 +9,7 @@ export const hashPassword = async (password) => {
     console.log(error);
   }
 };
-
+ 
 export const comparePassword = async (password, hashedPassword) => {
   return bcrypt.compare(password, hashedPassword);
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { registerController } from "../controller/authController.js";
+import { registerController , loginController, testController} from "../controller/authController.js";
+import {  isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
 
 
 //router object
@@ -9,5 +11,10 @@ const router = express.Router();
 //REGISTER || METHOD POST
 router.post("/register", registerController);
 
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
 
 export default router;
